9f1048a9b51eff962bae38e6a0faff1782e62bc0
Jim Wood <jwood5063@gmail.com>
2016-01-06 15:28:32 -0800

    Update delays

--- proj/hello/main.c
+++ proj/hello/main.c
@@ -31,14 +31,17 @@ int main(int argc, char **argv)
 	bcm2835_i2c_begin();
 
 	bcm2835_i2c_setSlaveAddress(0x50);
+	bcm2835_delayMicroseconds(100);
 
 	buffer[0]=CMD_CODE;
 	buffer[1]=CMD_ON;
 	bcm2835_i2c_write(buffer,2);
+	bcm2835_delayMicroseconds(100);
 
 	buffer[0]=CMD_CODE;
 	buffer[1]=CMD_DISP_I2C_ADDR;
 	bcm2835_i2c_write(buffer,2);
+	bcm2835_delayMicroseconds(100);
 
 	
 	bcm2835_i2c_end();
