6a8e6002e5c49e7db78b130c853b32ec69fe27bd
jwood5063 <jwood5063@gmail.com>
2016-01-18 14:25:41 -0800

    analog setup

--- proj/hello_arduino/main.c
+++ proj/hello_arduino/main.c
@@ -30,6 +30,17 @@ int main (void)
 			_delay_ms(200);
 		}
 	}
+   ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Set ADC prescalar to 128 - 125KHz sample rate @ 16MHz
+
+   ADMUX |= (1 << REFS0); // Set ADC reference to AVCC
+   ADMUX |= (1 << ADLAR); // Left adjust ADC result to allow easy 8 bit reading
+
+   // No MUX values needed to be changed to use ADC0
+
+   ADCSRA |= (1 << ADFR);  // Set ADC to Free-Running Mode
+
+   ADCSRA |= (1 << ADEN);  // Enable ADC
+   ADCSRA |= (1 << ADSC);  // Start A2D Conversions
 
 	return 0;
 }
