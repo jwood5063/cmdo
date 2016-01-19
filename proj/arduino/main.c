c08668930f0764935f10913a352e8644124a6f19
jwood5063 <=>
2016-01-19 02:59:36 +0000

    Add arduino project

---
+++ proj/arduino/main.c
@@ -0,0 +1,29 @@
+/* vim:ts=8:sw=8
+ * Copyright (c) 2012 Quantenna Communications, Inc.
+ * All rights reserved.
+ *
+ *  file purpose
+ *
+ */
+
+////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////
+#define GLOBAL	// indicate globals declared in this file
+#include <cmdo.h>
+#include <i2c.h>
+
+// hello world for i2c lcd display - no driver
+/***************************************************************************
+   Function:    
+   Purpose:    
+   Returns:
+   Note:    
+ **************************************************************************/
+int main(int argc, char **argv)
+{
+	u8 buffer[32];
+	char hello[]="hello";
+
+	return 0;
+}
