3c2e981d3f696100104111136172ca816d2734ba
jwood5063 <jwood5063@gmail.com>
2016-01-19 18:24:18 +0000

    Add flashy light routines

--- proj/hello_arduino/main.c
+++ proj/hello_arduino/main.c
@@ -29,6 +29,48 @@ int main (void)
 			PORTB = leds;
 			_delay_ms(200);
 		}
+		PORTB = 0;
+		_delay_ms(200);
+		for (leds = 0;leds < 6; leds++) {
+			PORTB = PORTB | BIT(leds);
+			_delay_ms(200);
+		}
+		for (leds = 0;leds < 6; leds++) {
+			PORTB = PORTB & ~BIT(leds);
+			_delay_ms(200);
+		}
+		PORTB = 0x21;
+		_delay_ms(200);
+		PORTB = 0x12;
+		_delay_ms(200);
+		PORTB = 0xc;
+		_delay_ms(200);
+		PORTB = 0x12;
+		_delay_ms(200);
+		PORTB = 0x21;
+		_delay_ms(200);
+		PORTB = 0;
+		for (leds = 0;leds < 0x40; leds++) {
+			PORTB = leds;
+			_delay_ms(200);
+		}
+		for (leds = 0;leds < 0x40; leds++) {
+			PORTB = rand()&0x3f;
+			_delay_ms(200);
+		}
+		for (leds = 1; leds <= (1 << 5); leds <<= 1) {
+			PORTB = leds;
+			_delay_ms(200);
+		}
+		for (leds = (1 << 4); leds >= 2; leds >>= 1) {
+			PORTB = leds;
+			_delay_ms(200);
+		}
+		PORTB = 0;
+		for (leds = 0;leds < 0x40; leds++) {
+			PORTB = BIT(rand()&0x7);
+			_delay_ms(200);
+		}
 	}
 
 	return 0;
